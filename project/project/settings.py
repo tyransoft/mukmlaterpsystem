ee3e37c4b4202baad0202cea3c1e0af2cf67f843
tyransoft <tyransoftagency@gmail.com>
2026-05-31 18:55:06 +0200

    asdfh

--- project/project/settings.py
+++ project/project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mukemlat',
+        'USER': 'webadmin',
+        'PASSWORD': 'SLPskg91411',
+        'HOST': '10.7.6.92',
+        'PORT': '5432',
     }
 }
 

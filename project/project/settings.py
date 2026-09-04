e6c0c8f66ce803671e867307c14d30ca25258bb2
tyransoft <tyransoftagency@gmail.com>
2026-09-04 03:17:26 +0200

    456

--- project/project/settings.py
+++ project/project/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'core.context_processors.company_settings',
-
+                'core.context_processors.product_expiry_dashboard',
             ],
         },
     },
